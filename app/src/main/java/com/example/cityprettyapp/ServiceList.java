657705b5e1f6c4c1e163098c7092c5144d564e5d
oddFax <tsmith47@memphis.edu>
2019-04-02 17:39:55 -0500

    So its a list now

--- app/src/main/java/com/example/cityprettyapp/ServiceList.java
+++ app/src/main/java/com/example/cityprettyapp/ServiceList.java
@@ -1,16 +1,9 @@
 package com.example.cityprettyapp;
 
-import android.app.Service;
-import android.database.DataSetObserver;
+
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -22,29 +15,33 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import org.w3c.dom.Text;
+
 
 import java.util.ArrayList;
-import java.util.List;
+
 
 
 public class ServiceList extends AppCompatActivity{
-    private TextView tv;
+
+    private ListView tv;
     DatabaseReference database;
     ArrayList<Services> s;
+    ArrayAdapter<Services> servList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.service_list);
-        tv = (TextView) findViewById(R.id.serviceExample);
+        tv = (ListView) findViewById(R.id.serviceExample);
         s = new ArrayList<>();
+        servList = new ArrayAdapter<Services>(this, android.R.layout.simple_list_item_1, s);
         database = FirebaseDatabase.getInstance().getReference("Services");
         database.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                tv.setText(dataSnapshot.getValue().toString());
-                /*for (DataSnapshot ds : dataSnapshot.getChildren()){
+
+                for (DataSnapshot ds : dataSnapshot.getChildren()){
                     ds.getValue().toString();
                     String name = ds.child("name").getValue().toString();
                     String price = ds.child("price").getValue().toString();
@@ -52,8 +49,8 @@ public class ServiceList extends AppCompatActivity{
                     String description = ds.child("description").getValue().toString();
                     Services newServ = new Services(name,price, description, id);
                     s.add(newServ);
-                }*/
-
+                }
+                tv.setAdapter(servList);
             }
 
             @Override
---
+++ app/src/main/java/com/example/cityprettyapp/ServiceListAdapter.java
@@ -0,0 +1,81 @@
+package com.example.cityprettyapp;
+
+import android.content.Context;
+import android.database.DataSetObserver;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ServiceListAdapter implements ListAdapter {
+    ArrayList<Services> arrayList;
+    Context context;
+    public ServiceListAdapter(Context context, ArrayList<Services> arrayList) {
+        this.arrayList=arrayList;
+        this.context=context;
+    }
+    @Override
+    public boolean areAllItemsEnabled() {
+        return false;
+    }
+    @Override
+    public boolean isEnabled(int position) {
+        return true;
+    }
+    @Override
+    public void registerDataSetObserver(DataSetObserver observer) {
+    }
+    @Override
+    public void unregisterDataSetObserver(DataSetObserver observer) {
+    }
+    @Override
+    public int getCount() {
+        return arrayList.size();
+    }
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+       /* Services subjectData=arrayList.get(position);
+        if(convertView==null) {
+            LayoutInflater layoutInflater = LayoutInflater.from(context);
+            convertView=layoutInflater.inflate(R.layout.list_row, null);
+            convertView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                }
+            });
+            TextView tittle=convertView.findViewById(R.id.title);
+            tittle.setText(subjectData.SubjectName);
+            Picasso.with(context)
+                    .load(subjectData.Image)
+                    .into(imag);
+        }*/
+        return convertView;
+    }
+    @Override
+    public int getItemViewType(int position) {
+        return position;
+    }
+    @Override
+    public int getViewTypeCount() {
+        return arrayList.size();
+    }
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
